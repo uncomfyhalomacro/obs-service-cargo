db694d73db64fd66e548ba0c02bbdddb2adb17a7
Soc Virnyl Estela <socvirnyl.estela@gmail.com>
2023-08-30 10:47:47 +0800

    split into separate function
    Signed-off-by: Soc Virnyl Estela <socvirnyl.estela@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use clap::Parser;
-use std::path::{Path, PathBuf};
+use std::{
+    path::{Path, PathBuf},
+    process::ExitCode,
+};
 
 use obs_service_cargo_vendor_rs::{
     cli::{Compression, Opts, Src, SrcKind},
@@ -7,16 +10,30 @@ use obs_service_cargo_vendor_rs::{
 };
 use tempfile;
 
-fn main() {
+fn main() -> ExitCode {
+    match run_cargo_vendor() {
+        Ok(ok) => ok,
+        Err(err) => err,
+    }
+}
+
+fn run_cargo_vendor() -> Result<ExitCode, ExitCode> {
     let args = Opts::parse();
-    match &args.get_srckind() {
+    let exit_status = match &args.srckind() {
         Some(kind) => {
             if matches!(kind, SrcKind::SrcTar) {
                 let srcpath = args
                     .srctar
                     .as_deref()
                     .expect("Source tar cannot be determined");
-                let compression_type = args.srctar_compression_type();
+                let compression_type = match args.srctar_compression_type() {
+                    Ok(t) => t,
+                    Err(err) => {
+                        eprintln!("{}", err);
+                        return Err(ExitCode::FAILURE);
+                    }
+                };
+
                 process_srctar(&srcpath, &compression_type);
             } else if matches!(kind, SrcKind::SrcDir) {
                 let srcpath = args
@@ -25,9 +42,12 @@ fn main() {
                     .expect("Source dir cannot be determined");
                 process_srcdir(&srcpath);
             }
+            Ok(ExitCode::SUCCESS)
         }
-        None => panic!("Not satisfied"),
-    }
+        None => Err(ExitCode::FAILURE),
+    };
+
+    exit_status
 }
 
 fn process_srctar(srctar: impl AsRef<Path>, compression_type: &Compression) {}
