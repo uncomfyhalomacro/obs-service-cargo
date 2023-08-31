ebcea5278af8f724ff64af7eddde81c4b4010e39
Soc Virnyl Estela <socvirnyl.estela@gmail.com>
2023-08-31 23:02:12 +0800

    boilerplate -> to be squashed before PR
    Signed-off-by: Soc Virnyl Estela <socvirnyl.estela@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use clap::Parser;
+use obs_service_cargo_vendor_rs::{
+    cli::{Compression, Opts, Src, SrcKind},
+    utils::{self, cargo_vendor},
+};
+use std::io::prelude::*;
 use std::{
+    fs,
     path::{Path, PathBuf},
     process::ExitCode,
 };
-
-use obs_service_cargo_vendor_rs::{
-    cli::{Compression, Opts, Src, SrcKind},
-    utils,
-};
+use tar;
 use tempfile;
 
 fn main() -> ExitCode {
@@ -25,7 +27,10 @@ fn run_cargo_vendor() -> Result<ExitCode, ExitCode> {
                 let srcpath = args
                     .srctar
                     .as_deref()
-                    .expect("Source tar cannot be determined");
+                    .expect("Source tar cannot be determined")
+                    .to_path_buf()
+                    .canonicalize()
+                    .unwrap();
                 let compression_type = match args.srctar_compression_type() {
                     Ok(t) => t,
                     Err(err) => {
@@ -34,13 +39,27 @@ fn run_cargo_vendor() -> Result<ExitCode, ExitCode> {
                     }
                 };
 
-                process_srctar(&srcpath, &compression_type);
+                let vendor_compression_type = args.compression;
+                let update: bool = args.update;
+
+                process_srctar(&srcpath, compression_type, vendor_compression_type, update);
             } else if matches!(kind, SrcKind::SrcDir) {
                 let srcpath = args
                     .srcdir
                     .as_deref()
-                    .expect("Source dir cannot be determined");
-                process_srcdir(&srcpath);
+                    .expect("Source dir cannot be determined")
+                    .to_path_buf()
+                    .canonicalize()
+                    .unwrap()
+                    .to_path_buf();
+
+                let vendor_compression_type = args.compression;
+                let update: bool = args.update;
+                let tag = args.tag;
+                let outdir = args.outdir;
+
+                // I wonder if this can be just a trait method? 🤔
+                process_srcdir(&srcpath, vendor_compression_type, update, tag, outdir);
             }
             Ok(ExitCode::SUCCESS)
         }
@@ -50,6 +69,47 @@ fn run_cargo_vendor() -> Result<ExitCode, ExitCode> {
     exit_status
 }
 
-fn process_srctar(srctar: impl AsRef<Path>, compression_type: &Compression) {}
+fn process_srctar(
+    srctar: impl AsRef<Path>,
+    srctar_compression_type: Compression,
+    vendor_compression_type: Compression,
+    update: bool,
+) {
+}
 
-fn process_srcdir(srcdir: impl AsRef<Path>) {}
+fn process_srcdir(
+    srcdir: impl AsRef<Path>,
+    vendor_compression_type: Compression,
+    update: bool,
+    tag: Option<String>,
+    outdir: impl AsRef<Path>,
+) {
+    match tempfile::Builder::new()
+        .prefix(".obs-service-cargo-vendor")
+        .rand_bytes(8)
+        .tempdir()
+    {
+        Ok(dir) => {
+            let basename = &srcdir.as_ref().file_name().expect("No basename");
+            let dir = dir.path().join(&basename);
+            utils::copy_dir_all(&srcdir, &dir).expect("Cannot copy");
+            let mut prjroot = utils::get_manifest_file(&dir)
+                .expect("Something went wrong")
+                .parent()
+                .expect("What is parent path?")
+                .to_owned();
+            utils::cargo_vendor(&prjroot).expect("Error. Cannot vendor");
+            let vendor_tar_path = format!("{}/vendor.tar", &outdir.as_ref().to_str().unwrap());
+            println!("{}", &vendor_tar_path);
+            let vendor_tar_path = fs::File::create(&vendor_tar_path).unwrap();
+            let mut ar = tar::Builder::new(vendor_tar_path);
+            prjroot.push("vendor");
+            ar.append_dir_all("vendor/", &prjroot).unwrap();
+            ar.finish().expect("Something wrong");
+        }
+        Err(err) => {
+            eprintln! {"{}", err};
+            panic!("Something went wrong!");
+        }
+    };
+}
