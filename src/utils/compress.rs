ecf48c077d84e707ba58d19491f366e126324aab
Soc Virnyl Estela <socvirnyl.estela@gmail.com>
2023-09-01 22:53:29 +0800

    chore: refactor compress utils to be generic
    it won't make sense if it only is used for vendoring. make it useful
    for some other stuff here too.
    
    Signed-off-by: Soc Virnyl Estela <socvirnyl.estela@gmail.com>

--- src/utils/compress.rs
+++ src/utils/compress.rs
@@ -3,53 +3,44 @@ use std::io;
 use std::path::Path;
 use tar;
 
-// NOTE: See https://docs.rs/async-compression/latest/async_compression
-// I doubt we even need that but just putting the link there in case
-pub fn targz(outdir: impl AsRef<Path>, prjroot: impl AsRef<Path>) -> Result<(), io::Error> {
+/*
+NOTE: See https://docs.rs/async-compression/latest/async_compression
+I doubt we even need that but just putting the link there in case
+TODO: Make this work for general stuff. Strcitly using it for vendor defeats its purpose
+*/
+pub fn targz(outdir: impl AsRef<Path>, srcpath: impl AsRef<Path>) -> Result<(), io::Error> {
     use flate2::write::GzEncoder;
     use flate2::Compression;
 
-    let mut vendorsrc = prjroot.as_ref().to_path_buf();
-    vendorsrc.push("vendor/");
-    let vendortar = fs::File::create(format!(
-        "{}/vendor.tar.gz",
-        &outdir.as_ref().to_str().unwrap()
-    ))?;
-    let enc = GzEncoder::new(vendortar, Compression::default());
+    let src = srcpath.as_ref().to_path_buf();
+    let outtar = fs::File::create(format!("{}", &outdir.as_ref().to_str().unwrap()))?;
+    let enc = GzEncoder::new(outtar, Compression::default());
     let mut tar = tar::Builder::new(enc);
-    tar.append_dir_all("vendor/", &vendorsrc)?;
+    tar.append_dir_all(".", &src)?;
     tar.finish()?;
     Ok(())
 }
 
-pub fn tarzst(outdir: impl AsRef<Path>, prjroot: impl AsRef<Path>) -> Result<(), io::Error> {
+pub fn tarzst(outdir: impl AsRef<Path>, srcpath: impl AsRef<Path>) -> Result<(), io::Error> {
     use zstd::stream::Encoder;
     use zstd::DEFAULT_COMPRESSION_LEVEL;
-    let mut vendorsrc = prjroot.as_ref().to_path_buf();
-    vendorsrc.push("vendor/");
-    let vendortar = fs::File::create(format!(
-        "{}/vendor.tar.zst",
-        &outdir.as_ref().to_str().unwrap()
-    ))?;
-    let enc = Encoder::new(vendortar, DEFAULT_COMPRESSION_LEVEL)?.auto_finish();
+    let src = srcpath.as_ref().to_path_buf();
+    let outtar = fs::File::create(format!("{}", &outdir.as_ref().to_str().unwrap()))?;
+    let enc = Encoder::new(outtar, DEFAULT_COMPRESSION_LEVEL)?.auto_finish();
     let mut tar = tar::Builder::new(enc);
-    tar.append_dir_all("vendor/", &vendorsrc)?;
+    tar.append_dir_all(".", &src)?;
     tar.finish()?;
     Ok(())
 }
 
-pub fn tarxz(outdir: impl AsRef<Path>, prjroot: impl AsRef<Path>) -> Result<(), io::Error> {
+pub fn tarxz(outdir: impl AsRef<Path>, srcpath: impl AsRef<Path>) -> Result<(), io::Error> {
     use xz2::write::XzEncoder;
 
-    let mut vendorsrc = prjroot.as_ref().to_path_buf();
-    vendorsrc.push("vendor/");
-    let vendortar = fs::File::create(format!(
-        "{}/vendor.tar.xz",
-        &outdir.as_ref().to_str().unwrap()
-    ))?;
-    let enc = XzEncoder::new(vendortar, 6);
+    let src = srcpath.as_ref().to_path_buf();
+    let outtar = fs::File::create(format!("{}", &outdir.as_ref().to_str().unwrap()))?;
+    let enc = XzEncoder::new(outtar, 6);
     let mut tar = tar::Builder::new(enc);
-    tar.append_dir_all("vendor/", &vendorsrc)?;
+    tar.append_dir_all(".", &src)?;
     tar.finish()?;
     Ok(())
 }
